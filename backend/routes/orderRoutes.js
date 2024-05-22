7402a941259c8a1b07dad76fee9205fa40809fe1
Mohammad Ali <92550270+mohammadali-amd@users.noreply.github.com>
2024-05-22 22:15:04 +0330

    order routes

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import { admin, protect } from '../middleWare/authMiddleware.js'
+import { addOrderItems, getMyOrders, getOrderById, updateOrderToPaid, updateOrderToDeliveres, getOrders } from '../controllers/orderController.js'
+
+const router = express.Router()
+
+router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders)
+router.route('/myorders').get(protect, getMyOrders)
+router.route('/:id').get(protect, admin, getOrderById)
+router.route('/:id/pay').put(protect, updateOrderToPaid)
+router.route('/:id/deliver').put(protect, updateOrderToDeliveres)
+
+export default router
