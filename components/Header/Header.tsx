0f438212ab2e9df38569afe4ade696f391dad53f
Mohammad Ali <92550270+mohammadali-amd@users.noreply.github.com>
2024-10-09 15:25:13 +0330

    fix bug: loginButton in mobile device bug

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import Link from 'next/link';
 import Search from './Search';
 import Logo from '../Logo';
@@ -19,14 +19,17 @@ const links = [
 const Header: React.FC = () => {
    const isClient = useIsClient()
 
-   const [isToggle, setIsToggle] = useState(false);
-   const [mobileMenu, setMobileMenu] = useState(false);
+   const [isToggleDesktop, setIsToggleDesktop] = useState(false);
+   const [isToggleMobile, setIsToggleMobile] = useState(false);
+   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-   const dropdownRef = useRef<HTMLDivElement | null>(null);
+   const dropdownRefDesktop = useRef<HTMLDivElement | null>(null);
+   const dropdownRefMobile = useRef<HTMLDivElement | null>(null);
    const mobileMenuRef = useRef<HTMLDivElement | null>(null);
 
-   useClickOutside(dropdownRef, () => setIsToggle(false));
-   useClickOutside(mobileMenuRef, () => setMobileMenu(false));
+   useClickOutside(dropdownRefDesktop, () => setIsToggleDesktop(false));
+   useClickOutside(dropdownRefMobile, () => setIsToggleMobile(false));
+   useClickOutside(mobileMenuRef, () => setMobileMenuOpen(false));
 
    // Detect scroll direction
    const isScrolledUp = useScrollDirection(50);
@@ -35,14 +38,14 @@ const Header: React.FC = () => {
       <header className={`px-5 sm:px-10 md:px-20 pt-2 lg:pt-5 pb-3 shadow-md border-b border-stone-300 sticky top-0 z-30 transition-all duration-200 bg-white ${isScrolledUp ? '-translate-y-16 md:-translate-y-0 lg:h-[6rem]' : 'lg:h-[8.5rem]'}`}>
          <div className={`flex items-center justify-between md:hidden`}>
             {/* Mobile Menu Button */}
-            <button onClick={() => setMobileMenu(!mobileMenu)}>
+            <button onClick={() => setMobileMenuOpen(!mobileMenuOpen)}>
                <i className="lni lni-menu text-2xl"></i>
             </button>
             {/* Logo */}
             <Logo />
             {/* Login Button */}
             {isClient && (
-               <LoginButton isToggle={isToggle} dropdownRef={dropdownRef} setIsToggle={setIsToggle} />
+               <LoginButton isToggle={isToggleMobile} dropdownRef={dropdownRefMobile} setIsToggle={setIsToggleMobile} />
             )}
          </div>
 
@@ -50,9 +53,7 @@ const Header: React.FC = () => {
             {/* Search */}
             <Search />
             {/* Cart */}
-            {isClient && (
-               <Cart />
-            )}
+            {isClient && (<Cart />)}
          </div>
 
          {/* Desktop Header */}
@@ -63,7 +64,7 @@ const Header: React.FC = () => {
             </div>
             {isClient && (
                <div className="flex items-center gap-8">
-                  <LoginButton isToggle={isToggle} dropdownRef={dropdownRef} setIsToggle={setIsToggle} />
+                  <LoginButton isToggle={isToggleDesktop} dropdownRef={dropdownRefDesktop} setIsToggle={setIsToggleDesktop} />
                   <Cart />
                </div>
             )}
@@ -81,13 +82,13 @@ const Header: React.FC = () => {
          </div>
 
          {/* Mobile Menu */}
-         <div ref={mobileMenuRef} className={`${mobileMenu ? 'translate-x-0' : 'translate-x-full'} ${isScrolledUp ? 'translate-y-12' : ''} fixed z-20 right-0 bottom-0 h-full w-4/5 bg-slate-100 shadow-2xl transition-transform duration-200`}>
-            <i onClick={() => setMobileMenu(false)} className="lni lni-close text-3xl absolute left-0 top-0 pl-6 pt-4 cursor-pointer"></i>
+         <div ref={mobileMenuRef} className={`${mobileMenuOpen ? 'translate-x-0' : 'translate-x-full'} ${isScrolledUp ? 'translate-y-12' : ''} fixed z-20 right-0 bottom-0 h-full w-4/5 bg-slate-100 shadow-2xl transition-transform duration-200`}>
+            <i onClick={() => setMobileMenuOpen(false)} className="lni lni-close text-3xl absolute left-0 top-0 pl-6 pt-4 cursor-pointer"></i>
             <div className="relative">
                <ul className="flex flex-col items-center content-start mt-16">
                   {links.map((link) => (
                      <li key={link.slug} className="py-5 text-xl">
-                        <Link href={link.slug} onClick={() => setMobileMenu(false)} className='hover:text-theme-color'>{link.name}</Link>
+                        <Link href={link.slug} onClick={() => setMobileMenuOpen(false)} className='hover:text-theme-color'>{link.name}</Link>
                      </li>
                   ))}
                </ul>
